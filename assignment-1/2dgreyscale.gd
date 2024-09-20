a50b4bca73ac381a2ba02aa41266224acedd2074
Kaiya Wangler <lilah1305@gmail.com>
2024-09-20 12:28:51 -0700

    Updated 2D Image using only cellular noise and fractal_octaves

--- assignment-1/2dgreyscale.gd
+++ assignment-1/2dgreyscale.gd
@@ -11,16 +11,12 @@ func _ready() -> void:
 func greyscale_image():
 	var cell_noise = FastNoiseLite.new()
 	cell_noise.set_noise_type(FastNoiseLite.TYPE_CELLULAR)
-	cell_noise.noise_type = 2
-	cell_noise.fractal_octaves = 4
-	cell_noise.frequency = 0.02
-	cell_noise.cellular_return_type = 3
-	cell_noise.cellular_return_type = 2
-	#noise.domain_warp_enabled = true
-	#noise.domain_warp_fractal_octaves = 6
-	#noise.domain_warp_fractal_lacunarity = 5
-	#noise.domain_warp_amplitude = 60
+	cell_noise.noise_type = 4
+	cell_noise.fractal_octaves = 6
+	cell_noise.frequency = 0.04
+	cell_noise.cellular_jitter = 1
 	cell_noise.cellular_distance_function = 2
+	cell_noise.cellular_return_type = 2
 	var image_noise = cell_noise.get_seamless_image(200,200)
 	var image = Image.create(200, 200, false, Image.FORMAT_RGB8)
 	for x in range (200):
